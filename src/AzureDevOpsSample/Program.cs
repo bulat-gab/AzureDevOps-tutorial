1a5b2f063c370ad61edc29dc380a7b3a2440a304
bulat-gab <gabdrakhmanov.br@gmail.com>
2021-09-10 19:06:24 +0300

    Add console app

---
+++ src/AzureDevOpsSample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AzureDevOpsSample
+{
+    class Program
+    {
+        [Test]
+        public void SuccessfulTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [Test]
+        public void FailingTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
